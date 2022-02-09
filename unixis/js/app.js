57926f377626722b2cba4b03fde7d631ffc822bf
Moshi <moshi.huang@gmail.com>
2022-02-10 01:31:16 +0800

    Add files via upload

---
+++ unixis/js/app.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+
+// jQuery
+$(function() {
+
+  $('.side-menu li').on('click', function() {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+
+});
+
